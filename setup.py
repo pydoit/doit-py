500731ce25e89f327d190b7b8b3fc02bbd71c0f1
Eduardo Schettino <schettino72@gmail.com>
2020-05-24 20:35:05 +0800

    bump version to 0.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ install_requires = [
 
 setup (
     name = 'doit-py',
-    version = '0.4.0',
+    version = '0.5.0',
     author = 'Eduardo Naufel Schettino',
     author_email = 'schettino72@gmail.com',
     description = 'doit tasks for python stuff',
